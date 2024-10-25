762f274eb3aea2bfd67ac4774f8accba2ea46268
G. MOMO <6159388+georgesmomo@users.noreply.github.com>
2024-10-25 10:57:55 +0100

    erreur vercel: TypeError: Cannot read properties of undefined (reading 'env'), on ajoute process devant env

--- app/lib/.server/llm/api-key.ts
+++ app/lib/.server/llm/api-key.ts
@@ -7,7 +7,7 @@ export function getAPIKey(cloudflareEnv: Env, provider: string) {
    * The `cloudflareEnv` is only used when deployed or when previewing locally.
    * In development the environment variables are available through `env`.
    */
-
+/* on adapte pour faire fonctionner sur vercel, on ajoute process devant env
   switch (provider) {
     case 'Anthropic':
       return env.ANTHROPIC_API_KEY || cloudflareEnv.ANTHROPIC_API_KEY;
@@ -39,4 +39,38 @@ export function getBaseURL(cloudflareEnv: Env, provider: string) {
     default:
       return "";
   }
+}
+  */
+
+switch (provider) {
+  case 'Anthropic':
+    return process.env.ANTHROPIC_API_KEY || cloudflareEnv.ANTHROPIC_API_KEY;
+  case 'OpenAI':
+    return process.env.OPENAI_API_KEY || cloudflareEnv.OPENAI_API_KEY;
+  case 'Google':
+    return process.env.GOOGLE_GENERATIVE_AI_API_KEY || cloudflareEnv.GOOGLE_GENERATIVE_AI_API_KEY;
+  case 'Groq':
+    return process.env.GROQ_API_KEY || cloudflareEnv.GROQ_API_KEY;
+  case 'OpenRouter':
+    return process.env.OPEN_ROUTER_API_KEY || cloudflareEnv.OPEN_ROUTER_API_KEY;
+  case 'Deepseek':
+    return process.env.DEEPSEEK_API_KEY || cloudflareEnv.DEEPSEEK_API_KEY
+  case 'Mistral':
+    return process.env.MISTRAL_API_KEY || cloudflareEnv.MISTRAL_API_KEY;        
+  case "OpenAILike":
+    return process.env.OPENAI_LIKE_API_KEY || cloudflareEnv.OPENAI_LIKE_API_KEY;
+  default:
+    return "";
+}
+}
+
+export function getBaseURL(cloudflareEnv: Env, provider: string) {
+switch (provider) {
+  case 'OpenAILike':
+    return process.env.OPENAI_LIKE_API_BASE_URL || cloudflareEnv.OPENAI_LIKE_API_BASE_URL;
+  case 'Ollama':
+      return process.env.OLLAMA_API_BASE_URL || cloudflareEnv.OLLAMA_API_BASE_URL || "http://localhost:11434";
+  default:
+    return "";
+}
 }
