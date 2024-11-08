7310ef05c890d7d093a7a2cb3d5af1e7f6aeeca6
G. MOMO <6159388+georgesmomo@users.noreply.github.com>
2024-11-08 09:45:25 +0100

    Update api-key.ts
    Add process. to correct erreur on vercel

--- app/lib/.server/llm/api-key.ts
+++ app/lib/.server/llm/api-key.ts
@@ -10,21 +10,21 @@ export function getAPIKey(cloudflareEnv: Env, provider: string) {
 /* on adapte pour faire fonctionner sur vercel, on ajoute process devant env
   switch (provider) {
     case 'Anthropic':
-      return env.ANTHROPIC_API_KEY || cloudflareEnv.ANTHROPIC_API_KEY;
+      return process.env.ANTHROPIC_API_KEY || cloudflareEnv.ANTHROPIC_API_KEY;
     case 'OpenAI':
-      return env.OPENAI_API_KEY || cloudflareEnv.OPENAI_API_KEY;
+      return process.env.OPENAI_API_KEY || cloudflareEnv.OPENAI_API_KEY;
     case 'Google':
-      return env.GOOGLE_GENERATIVE_AI_API_KEY || cloudflareEnv.GOOGLE_GENERATIVE_AI_API_KEY;
+      return process.env.GOOGLE_GENERATIVE_AI_API_KEY || cloudflareEnv.GOOGLE_GENERATIVE_AI_API_KEY;
     case 'Groq':
-      return env.GROQ_API_KEY || cloudflareEnv.GROQ_API_KEY;
+      return process.env.GROQ_API_KEY || cloudflareEnv.GROQ_API_KEY;
     case 'OpenRouter':
-      return env.OPEN_ROUTER_API_KEY || cloudflareEnv.OPEN_ROUTER_API_KEY;
+      return process.env.OPEN_ROUTER_API_KEY || cloudflareEnv.OPEN_ROUTER_API_KEY;
     case 'Deepseek':
-      return env.DEEPSEEK_API_KEY || cloudflareEnv.DEEPSEEK_API_KEY
+      return process.env.DEEPSEEK_API_KEY || cloudflareEnv.DEEPSEEK_API_KEY
     case 'Mistral':
-      return env.MISTRAL_API_KEY || cloudflareEnv.MISTRAL_API_KEY;        
+      return process.env.MISTRAL_API_KEY || cloudflareEnv.MISTRAL_API_KEY;        
     case "OpenAILike":
-      return env.OPENAI_LIKE_API_KEY || cloudflareEnv.OPENAI_LIKE_API_KEY;
+      return process.env.OPENAI_LIKE_API_KEY || cloudflareEnv.OPENAI_LIKE_API_KEY;
     default:
       return "";
   }
@@ -33,9 +33,9 @@ export function getAPIKey(cloudflareEnv: Env, provider: string) {
 export function getBaseURL(cloudflareEnv: Env, provider: string) {
   switch (provider) {
     case 'OpenAILike':
-      return env.OPENAI_LIKE_API_BASE_URL || cloudflareEnv.OPENAI_LIKE_API_BASE_URL;
+      return process.env.OPENAI_LIKE_API_BASE_URL || cloudflareEnv.OPENAI_LIKE_API_BASE_URL;
     case 'Ollama':
-        return env.OLLAMA_API_BASE_URL || cloudflareEnv.OLLAMA_API_BASE_URL || "http://localhost:11434";
+        return process.env.OLLAMA_API_BASE_URL || cloudflareEnv.OLLAMA_API_BASE_URL || "http://localhost:11434";
     default:
       return "";
   }
